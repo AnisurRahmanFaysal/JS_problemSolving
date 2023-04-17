951802331fdc13b5d491d57c352e8374eeb92e03
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2023-04-17 15:56:26 +0600

    Assignment-3

--- bohobrihi/JS/assignment3.js
+++ bohobrihi/JS/assignment3.js
@@ -1,33 +1,62 @@
-var number1,number2,menu,Case;
+console.log("Select an Option\n1.Add\n2.Sub\n3.Mul\n4.Dev");
 
-number1 = parseInt(prompt("Enter First Number:"));
-number2 = parseInt(prompt("Enter First Number:"));
+var number1, number2, option, C1, C2, C3,result;
 
-menu = ["1.Add","2.Sub","3.Mul","4.Dev"];
+number1 = parseInt(prompt("Enter Values:"));
+number2 = parseInt(prompt("Enter Values:"));
+option = parseInt(prompt("Enter Your Choice:"));
 
-menu.forEach(element => {
-    console.log(element);
-});
+C1 = isNaN(number1);
+C2 = isNaN(number2);
+C3 = isNaN(option);
 
-Case = parseInt(prompt("What is your Choice:"))
+if(C1 || C2 || C3){
+    console.log("Invalid Number");
+}
+else{
+    switch(option){
+        case 1:
+            result = number1+number2;
+            break;
 
-var result;
+        case 2:
+            result = number1-number2;
+            break;
+        case 3:
+            result = number1*number2;
+            break;
 
-switch(Case){
-    case 1:
-        result = number1+number2;
-        break;
+        case 4:
+            result = number1/number2;
+            break;
+    }
 
-    case 2:
-        result = number1-number2;
-        break;
-    case 3:
-        result = number1*number2;
-        break;
-
-    case 4:
-        result = number1/number2;
-        break;
+    console.log(result);
 }
 
-console.log(result);
+
+/* <p>var number1,number2,menu,Case;
+
+        menu = ["1.Add","2.Sub","3.Mul","4.Dev"];
+        menu.forEach(element => {
+            console.log(element);
+        });
+        
+        number1 = parseInt(prompt("Enter First Number:"));
+        number2 = parseInt(prompt("Enter First Number:"));
+        
+        
+        
+        var numberCon1 = isNan(number1);
+        var numberCon2 = isNan(number2);
+        
+        Case = parseInt(prompt("What is your Choice:"));
+        var CaseCon = isNan(Case);
+        
+        
+        if(numberCon1 || numberCon || CaseCon){
+            console.log("Invalid Number");
+        }
+        else{
+            
+        }</p> */
